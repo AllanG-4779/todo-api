a54d0d648790f37bfc1861940b1c29d4fcdbaa3c
AllanG <allang4779@gmail.com>
2021-09-25 10:43:53 +0300

    allowed hosts config

--- django_rest/settings.py
+++ django_rest/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jy8fp2#nbuss(6m!rmqli*v!_1+#jr)m8idgn*q5k0lyy81os2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://allang-todo.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
